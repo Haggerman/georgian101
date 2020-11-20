e5b40606fc26330b2948998f57ec98f2f759b825
Haggerman <jan.ther@seznam.cz>
2020-11-20 19:58:18 +0100

    Test obrazku

--- georgianApp.py
+++ georgianApp.py
@@ -1,35 +1,92 @@
-import flask
-from flask import flash, request, redirect
-import numpy
-import cv2
+import os
+import subprocess
+import sys
+import logging
+import shutil
+from flask import Flask, jsonify, render_template, request
+from werkzeug import secure_filename
 
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['TEMP_FOLDER'] = '/tmp'
+app.config['OCR_OUTPUT_FILE'] = 'ocr'
+app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
 
-georgianApp = flask.Flask(__name__)
+def allowed_file(filename):
+  return '.' in filename and filename.rsplit('.', 1)[1] in set(['png', 'jpg', 'jpeg', 'gif', 'tif', 'tiff'])
 
-# def allowed_file(filename):
-#     return '.' in filename and \
-#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+@app.errorhandler(404)
+def not_found(error):
+  resp = jsonify( {
+    u'status': 404,
+    u'message': u'Resource not found'
+  } )
+  resp.status_code = 404
+  return resp
 
+@app.route('/')
+def api_root():
+  resp = jsonify( {
+    u'status': 200,
+    u'message': u'Welcome to our secret APIs'
+  } )
+  resp.status_code = 200
+  return resp
 
-@georgianApp.route('/', methods=['GET','POST'])
-def home():
-    a = numpy.zeros(shape=(5, 2))
-    return str(a[1][1])
+@app.route('/test', methods = ['GET'])
+def test():
+  return render_template('upload_form.html', landing_page = 'process')
 
+@app.route('/process', methods = ['GET','POST'])
+def process():
+  if request.method == 'POST':
+    file = request.files['file']
+    hocr = request.form.get('hocr') or ''
+    ext = '.hocr' if hocr else '.txt'
+    if file and allowed_file(file.filename):
+      folder = os.path.join(app.config['TEMP_FOLDER'], str(os.getpid()))
+      os.mkdir(folder)
+      input_file = os.path.join(folder, secure_filename(file.filename))
+      output_file = os.path.join(folder, app.config['OCR_OUTPUT_FILE'])
+      file.save(input_file)
 
-if __name__ == "__main__":
-    georgianApp.run()
-        # if request.method == 'POST':
-        #     # check if the post request has the file part
-        #     if 'file' not in request.files:
-        #         flash('No file part')
-        #         return redirect(request.url)
-        #     file = request.files['file']
-        #     # if user does not select file, browser also
-        #     # submit a empty part without filename
-        #     if file.filename == '':
-        #         flash('No selected file')
-        #         return redirect(request.url)
-        #     if file and allowed_file(file.filename):
-        #         flash('Aaaa')
+      command = ['tesseract', input_file, output_file, '-l', request.form['lang'], hocr]
+      proc = subprocess.Popen(command, stderr=subprocess.PIPE)
+      proc.wait()
+
+      output_file += ext
+
+      if os.path.isfile(output_file):
+        f = open(output_file)
+        resp = jsonify( {
+          u'status': 200,
+          u'ocr':{k:v.decode('utf-8') for k,v in enumerate(f.read().splitlines())}
+        } )
+      else:
+        resp = jsonify( {
+          u'status': 422,
+          u'message': u'Unprocessable Entity'
+        } )
+        resp.status_code = 422
+
+      shutil.rmtree(folder)
+      return resp
+    else:
+      resp = jsonify( {
+        u'status': 415,
+        u'message': u'Unsupported Media Type'
+      } )
+      resp.status_code = 415
+      return resp
+  else:
+    resp = jsonify( {
+      u'status': 405,
+      u'message': u'The method is not allowed for the requested URL'
+    } )
+    resp.status_code = 405
+    return resp
+
+if __name__ == '__main__':
+  app.run(debug=True)
