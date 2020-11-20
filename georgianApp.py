9d860c8d2c8cbde47d898bd60aee8b49d6527f98
Haggerman <jan.ther@seznam.cz>
2020-11-20 13:38:18 +0100

    Test obrazku

--- georgianApp.py
+++ georgianApp.py
@@ -1,31 +1,34 @@
 import flask
 from flask import flash, request, redirect
-#import numpy
-#import cv2
+import numpy
+import cv2
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 georgianApp = flask.Flask(__name__)
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# def allowed_file(filename):
+#     return '.' in filename and \
+#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @georgianApp.route('/', methods=['GET','POST'])
 def home():
-        if request.method == 'POST':
-            # check if the post request has the file part
-            if 'file' not in request.files:
-                flash('No file part')
-                return redirect(request.url)
-            file = request.files['file']
-            # if user does not select file, browser also
-            # submit a empty part without filename
-            if file.filename == '':
-                flash('No selected file')
-                return redirect(request.url)
-            if file and allowed_file(file.filename):
-                print('a')
+    return "check opencv"
 
 
-
+if __name__ == "__main__":
+    georgianApp.run()
+        # if request.method == 'POST':
+        #     # check if the post request has the file part
+        #     if 'file' not in request.files:
+        #         flash('No file part')
+        #         return redirect(request.url)
+        #     file = request.files['file']
+        #     # if user does not select file, browser also
+        #     # submit a empty part without filename
+        #     if file.filename == '':
+        #         flash('No selected file')
+        #         return redirect(request.url)
+        #     if file and allowed_file(file.filename):
+        #         flash('Aaaa')
