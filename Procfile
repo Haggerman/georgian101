500a9b7a5573fed8ca9632357956bb75bc6d9da7
Haggerman <jan.ther@seznam.cz>
2020-11-20 10:22:59 +0100

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn georgianApp:app
---
+++ georgianApp.py
@@ -0,0 +1,14 @@
+import flask
+from flask import request
+
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    year = request.args['year']
+    try:
+        return year
+    except KeyError:
+        return 'Invalid input'
+
