a6d158595966da7d4746a47d48154a3150f7cd6f
Haggerman <jan.ther@seznam.cz>
2020-11-22 13:19:40 +0100

    Choose picture areas

--- server.py
+++ server.py
@@ -17,11 +17,11 @@ def home(request: Request):
 
 
 @app.post("/api/v1/extract_text")
-async def extract_text(image: UploadFile = File(...), coordinates: str = Form(...)):
+async def extract_text(image: UploadFile = File(...), coordinates: Optional[List[str]] = Form(...)):
     temp_file = _save_file_to_disk(image, path="temp", save_as="temp")
     text = await read_image(temp_file)
 
-    return {"filename": image.filename, "text": text, "coordinates": coordinates}
+    return {"filename": image.filename, "text": text, "coordinates": coordinates[1]}
 
 
 def _save_file_to_disk(uploaded_file, path=".", save_as="default"):
